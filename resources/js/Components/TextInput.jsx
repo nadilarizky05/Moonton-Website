3f741eeab09e574a314e2e2626c487f1be3316bf
Nadila Rizky Amelia <nadilarizkyamelia5@gmail.com>
2025-05-02 08:11:08 +0800

    feat: slicing sign in and sign up

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,7 +1,17 @@
 import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
+import PropTypes from 'prop-types';
 
-export default forwardRef(function TextInput(
-    { type = 'text', className = '', isFocused = false, ...props },
+const TextInput = forwardRef(function TextInput(
+    { 
+        type = 'text', 
+        defaultValue,
+        className = '', 
+        variant ='primary',
+        isFocused = false, 
+        placeholder,
+        isError,
+        ...props 
+    },
     ref,
 ) {
     const localRef = useRef(null);
@@ -20,11 +30,27 @@ export default forwardRef(function TextInput(
         <input
             {...props}
             type={type}
-            className={
-                'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 ' +
-                className
-            }
+            defaultValue={defaultValue}
+            className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full ${isError && "input-error"} input-${variant} ${className}`}
             ref={localRef}
+            placeholder={placeholder}
         />
     );
 });
+
+TextInput.propTypes = {
+    type: PropTypes.oneOf(['text', 'email', 'password', 'number', 'file']),
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    variant: PropTypes.oneOf(["primary", "error", "primary-outline"]),
+    autoComplete: PropTypes.string,
+    required: PropTypes.bool,
+    isFocused: PropTypes.bool,
+    handleChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    isError: PropTypes.bool,
+};
+
+export default TextInput;
