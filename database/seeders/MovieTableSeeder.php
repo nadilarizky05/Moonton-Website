8a871bbebd73d9419cdf3b6bd02c76d89b2d2f96
Nadila Rizky Amelia <nadilarizkyamelia5@gmail.com>
2025-07-29 20:01:14 +0800

    feat: integration dashboard

--- database/seeders/MovieTableSeeder.php
+++ database/seeders/MovieTableSeeder.php
@@ -15,31 +15,31 @@ class MovieTableSeeder extends Seeder
     {
         $movies = [
             [
-                'name' => 'The Shawshank Redemption',
-                'slug' => 'the-shawshank-redemption',
+                'name' => 'Upin Ipin Keris Siamang Tunggal',
+                'slug' => 'upin-ipin-keris-siamang-tunggal',
+                'category' => 'Adventure',
+                'video_url' => 'https://www.youtube.com/watch?v=wM8ic6PNMYA',
+                'thumbnail' => 'https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/7511/1457511-i-2911ebb1afc8',
+                'rating' => 4.3,
+                'is_featured' => 0
+            ],
+            [
+                'name' => 'Jumbo',
+                'slug' => 'jumbo',
                 'category' => 'Drama',
-                'video_url' => '',
-                'thumbnail' => '',
-                'rating' => 9.3,
+                'video_url' => 'https://www.youtube.com/watch?v=yMqDgbZmBdk',
+                'thumbnail' => 'https://raciniga.com/wp-content/uploads/2025/02/Film-Animasi-JUMBO-Resmi-Merilis-Poster-dan-Trailer.png',
+                'rating' => 4.2,
+                'is_featured' => 0
+            ],
+            [
+                'name' => 'Moana 2',
+                'slug' => 'moana-2',
+                'category' => 'Adventure',
+                'video_url' => 'https://www.youtube.com/watch?v=LKFuXETZUsI',
+                'thumbnail' => 'https://cdn.rri.co.id/berita/Atambua/o/1731159523803-NSBUC46XCJF2JPAICEHSCJWLPY/cexfp76x1si0r99.avif',
+                'rating' => 4.5,
                 'is_featured' => 1
-            ],
-            [
-                'name' => 'The Godfather',
-                'slug' => 'the-godfather',
-                'category' => 'Drama',
-                'video_url' => '',
-                'thumbnail' => '',
-                'rating' => 9.2,
-                'is_featured' => 0
-            ],
-            [
-                'name' => 'The Godfather: Part II',
-                'slug' => 'the-godfather-part-ii',
-                'category' => 'Drama',
-                'video_url' => '',
-                'thumbnail' => '',
-                'rating' => 9.0,
-                'is_featured' => 0
             ],
         ];
         Movie::insert($movies);
