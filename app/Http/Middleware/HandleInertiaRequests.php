f5ec3b2b5ed71b8a75a573ad750cfd62b288d024
Nadila Rizky Amelia <nadilarizkyamelia5@gmail.com>
2025-08-12 17:30:26 +0800

    feat: Admin Store Movie

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -6,6 +6,7 @@ use Auth;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Session;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -54,6 +55,10 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
                 'activePlan' => $this->activePlan()
+            ],
+            'flashMessage' => [
+                'message' => Session::get('message'),
+                'type' => Session::get('type')
             ],
         ];
     }
---
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -0,0 +1,17 @@
+import Authenticated from "@/Layouts/Authenticated/Index"
+import Button from "@/Components/PrimaryButton"
+import FlashMessage from "@/Components/FlashMessage"
+import { Link } from "@inertiajs/react"
+
+export default function Index({auth, flashMessage}) {
+    return (
+        <Authenticated auth={auth}>
+            <Link href={route('admin.dashboard.movie.create')}>
+                <Button type="button" className="w-40 mb-8">Insert New Movie</Button>    
+            </Link>   
+            {flashMessage?.message && (
+                <FlashMessage message={flashMessage.message} />     
+            )}
+        </Authenticated>
+    )
+}
