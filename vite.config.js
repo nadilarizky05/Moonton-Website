f3535137aae35ab673bfdb6f8a190bdd0c830927
Nadila Rizky Amelia <nadilarizkyamelia5@gmail.com>
2025-08-18 20:47:49 +0800

    Fix flickity dependency conflict 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: './',
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: './resources/js/app.jsx'
+            }
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
